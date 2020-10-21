ccc8845ee7ea85cabcbb8813bca2bea4d39015bf
Jasper Pas <jasper.pas@student.kuleuven.be>
2020-10-21 14:30:55 +0200

    update colorrecognition

--- Testen/testcolorrecognition.py
+++ Testen/testcolorrecognition.py
@@ -10,8 +10,8 @@ while(1):
     # Convert BGR to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     # define range of blue color in HSV
-    lower_blue = np.array([0, 0,0])
-    upper_blue = np.array([360,100,100])
+    lower_blue = np.array([94, 80, 2])
+    upper_blue = np.array([120, 255, 255])
     # Threshold the HSV image to get only blue colors
     mask = cv.inRange(hsv, lower_blue, upper_blue)
     # Bitwise-AND mask and original image
