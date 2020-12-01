5ac34852eb59b6dcd162b9e082d049d259d7d15e
Jasper Pas <jasper.pas@student.kuleuven.be>
2020-12-01 16:36:21 +0100

    persondetection 1/12

--- code_for_kinect/eigenmaskdetection/maskdetectioncolor.py
+++ code_for_kinect/eigenmaskdetection/maskdetectioncolor.py
@@ -2,23 +2,26 @@ import functions
 import cv2
 import numpy as np
 import time
+import imutils
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 # To use a video file as input
-# cap = cv2.VideoCapture('filename.mp4')
+cap = cv2.VideoCapture('color_2_without_skelet.mp4')
 
 while True:
     timer = cv2.getTickCount()
     _, img = cap.read()
-    img = functions.resize_image(img,100)
+    #img = imutils.resize(img, width=320)
+    img = functions.resize_image(img,50)
     img = cv2.flip(img, 1)
     faces1 = functions.detect_faces(img)
     faces, eyes = functions.true_eyes_and_faces(img)
     functions.add_rectangle(img, faces)
     functions.mask_due_color(img, faces)
-    fps = int(cv2.getTickFrequency()/(cv2.getTickCount()-timer))
-    cv2.putText(img, str(fps), (50,50),cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0))
-    img = functions.resize_image(img, 100)
+    #img = imutils.resize(img, width=640)
+    #img = functions.resize_image(img, 200)
+    fps = int(cv2.getTickFrequency() / (cv2.getTickCount() - timer))
+    cv2.putText(img, str(fps), (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0))
     cv2.imshow('img', img)
     # Stop if escape key is pressed
     k = cv2.waitKey(30) & 0xff
